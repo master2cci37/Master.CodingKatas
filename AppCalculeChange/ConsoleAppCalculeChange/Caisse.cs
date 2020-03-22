13774df80b409bd0e01276f704a91f364f477622
Master 2 CCI Tours <jf.pacory@laposte.net>
2020-03-22 19:16:53 +0100

    Add files via upload

---
+++ AppCalculeChange/ConsoleAppCalculeChange/Caisse.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculeChange
+{
+    public enum Valeur
+    {
+        Cent, DeuxCents, CinqCents, DixCents, VingtCents,
+        CinquanteCents, UnEuro, DeuxEuros, CinqEuros, DixEuros,
+        VingtEuros, CinquanteEuros, CentEuros, DeuxCentsEuros,
+        CinqCentsEuros
+    }
+
+    public class Caisse
+    {
+        public Change[] RetourneChange(decimal prix, decimal paiement)
+        {
+            return null;
+        }
+
+
+    }
+
+    public class Change
+    {
+        public int Quantite { get; set; }
+        public Valeur Valeur { get; set; }
+
+    }
+}
---
+++ AppCalculeChange/UnitTestProjectCalculeChange/CaisseTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleAppCalculeChange;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppCalculeChange.Tests
+{
+    [TestClass()]
+    public class CaisseTests
+    {
+        [TestMethod()]
+        public void RetourneChange1010Test()
+        {
+            var caisse = new Caisse();
+            var resultat = caisse.RetourneChange(10, 10);
+            Change[] attendu=null;
+            Assert.AreEqual(attendu, resultat);
+        }
+
+        /* tests à  valider 
+         10  /  10  => 0 -
+
+ 0.85 /  1.00 => 1   CinqCents,  1  DixCents,
+
+  1.95 / 5  =>  3 UnEuros,  1 CinquanteCents
+
+ 22.50  / 50  => 1 VingEuros, 1 CinqEuros, 1 DeuxEuros,  1 CinquanteCents
+
+ 13.09 /  200.00 =>       1  CentEuros,     1 CinquanteEuros       1 VingtEuros,       1 DixEuros,         1 CinqEuros,     1  UnEuro,          1 CinquanteCents,    2 VingtCents,        1 Cent 
+         * */
+    }
+}
